3aea83cbffd653000c4c5bf05b9579dc3aa46417
KariukiAntony <antonygichoya9@gmail.com>
2025-01-07 09:06:13 +0300

    Add: Version 2

--- app/app.py
+++ app/app.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 def json_response(data):
     response = {
-        "_version": "VERSION 1",
+        "_version": "VERSION 2",
         "_host": socket.gethostname(),
         "_time": time.time(),
         "_data": data
